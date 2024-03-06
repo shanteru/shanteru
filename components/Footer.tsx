b0b154e6460222416cbf87cbdffcca8391d768fc
shanteru <chantelleywloh@gmail.com>
2024-03-06 23:58:31 +0800

    Colour changed

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -16,7 +16,7 @@ export default async function Footer() {
         <div className="name flex flex-col items-center justify-center gap-x-4 gap-y-2 sm:flex-row sm:justify-self-start">
           <Link
             href="/"
-            className="text-xl font-extrabold tracking-tighter text-slate-100 transition-colors duration-150 hover:text-yellow-400"
+            className="text-xl font-extrabold tracking-tighter text-slate-100 transition-colors duration-150 hover:text-blue-400"
           >
             {settings.data.name}
           </Link>
@@ -37,7 +37,7 @@ export default async function Footer() {
                 <li>
                   <PrismicNextLink
                     className={clsx(
-                      "group relative block overflow-hidden  rounded px-3 py-1 text-base font-bold text-slate-100 transition-colors duration-150 hover:hover:text-yellow-400",
+                      "group relative block overflow-hidden  rounded px-3 py-1 text-base font-bold text-slate-100 transition-colors duration-150 hover:hover:text-blue-400",
                     )}
                     field={link}
                   >
